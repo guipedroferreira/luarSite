a53b1d401a1b71b5f60bf7017570d637e7c78641
joaopedroleonel <joaopsegatto@gmail.com>
2024-11-18 13:47:58 -0300

    oferta consertado

--- js/Produtos.js
+++ js/Produtos.js
@@ -4,13 +4,12 @@ window.addEventListener("load", () => {
 
 async function getprodutos() {
     try {
-        const respostaprodutos = await fetch("https://t93pg6q8.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type%3D%3D%27produto%27%5D%7BAvaliacao%2C+Images%5B0%5D%7Basset+-%3E+%7Burl%7D%7D%2C+Nome%2C+_id%7D", {
+        const respostaprodutos = await fetch("https://t93pg6q8.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type%3D%3D%27produto%27%5D%7BPreco%2C+Images%5B0%5D%7Basset+-%3E+%7Burl%7D%7D%2C+Nome%2C+_id%7D", {
             method: "GET"
         });
         const json = await respostaprodutos.json();
-        console.log(json.result)
+
         for(let i = 0; i < json.result.length; i++){
-            console.log("chamou")
             montarprodutos(json.result[i]);
         }
     } catch (error) {
@@ -23,23 +22,53 @@ function montarprodutos(data) {
     divprodutos.classList.add("col-4");
     
     const linkprodutos = document.createElement("a");
-    linkprodutos.href = "produto.html";
-    
-    const titulo = document.createElement("h4");
-    titulo.innerText = data.Nome;
-    titulo.classList.add("mt-[5px]");
-    
+    linkprodutos.href = `produto.html?id=${data._id}`;
+
     const imagens = document.createElement("img");
     imagens.classList.add("shadow-default");
     imagens.src = data.Images.asset.url;
+    imagens.style.aspectRatio = "1/1";
+    imagens.style.objectFit = "cover";
     linkprodutos.appendChild(imagens);
+    
+    const titulo = document.createElement("h4");
+    titulo.innerText = data.Nome;
+    titulo.classList.add("mt-[10px]");
+
+    const preco = document.createElement("p");
+    preco.innerText = "R$ " + formatarPreco(data.Preco);
 
     const button = document.createElement("a");
     button.innerText = "Entar em contato";
-    button.classList.add("bg-[#89A992]", "rounded-[50px]", "px-3", "py-3", "text-[white]");
+    button.classList.add("bg-[#89A992]", "rounded-[50px]", "btn", "mt-[10px]", "text-[white]");
     button.href = `https://api.whatsapp.com/send/?phone=5545920013524&text=Olá, gostaria de saber mais sobre a ${data.Nome}.&type=phone_number&app_absent=0`;
-    divprodutos.append(linkprodutos, titulo, button);
+    divprodutos.append(linkprodutos, titulo, preco, button);
 
     document.getElementById("velas").appendChild(divprodutos);
-    console.log(divprodutos);
+}
+
+
+function formatarPreco(precoOriginal) {
+
+    let precoEmTexto = precoOriginal.toString();
+    let decimal = precoEmTexto.match(/(?<=\.)\d+/g);
+
+    if(decimal) {
+
+      if(Number(decimal) < 10) {
+  
+        return (precoEmTexto + '0').replace(/\./g, ',');
+  
+      } else {
+  
+        return precoEmTexto.replace(/\./g, ',');
+  
+      }
+  
+    } else {
+
+      return precoEmTexto + ',00';
+
+    }
+
 }
--- js/oferta.js
+++ js/oferta.js
@@ -4,24 +4,20 @@ window.addEventListener("load", (event) => {
 
 async function getOferta() {
     try {
-        const respostaOferta = await fetch("https://t93pg6q8.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type%3D%3D%27novalinha%27%5D%5B0%5D%7BMensagem%2C+ProdutoNova+Linha+-%3E+%7BNome%2C+Imagens%5B0%5D%7Basset+-%3E+%7Burl%7D%7D%7D%7D", {
+        const respostaOferta = await fetch("https://t93pg6q8.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type%3D%3D%27novalinha%27%5D%5B0%5D%7BMensagem%2C+ProdutoNovaLinha+-%3E+%7BNome%2C+Images%5B0%5D%7Basset+-%3E+%7Burl%7D%7D%7D%7D", {
             method: "GET"
         });
 
         const dadosOferta = await respostaOferta.json();
 
-        if (dadosOferta.result.length > 0) {
-            const ofertaData = dadosOferta.result[0];
-            montarOferta(ofertaData);
-        } else {
-            console.error("Nenhuma oferta encontrada.");
-        }
+        montarOferta(dadosOferta.result);
+    
     } catch (error) {
         console.error("Erro ao buscar dados da API:", error.message);
     }
 }
 
-function montarOferta(oferta) {
+function montarOferta(data) {
     const offerDiv = document.createElement("div");
     offerDiv.classList.add("offer");
 
@@ -35,7 +31,7 @@ function montarOferta(oferta) {
     col1.classList.add("col-2");
 
     const offerImg = document.createElement("img");
-    offerImg.src = oferta.ProdutoNova.Linha[0].Imagens[0].asset.url;
+    offerImg.src = data.ProdutoNovaLinha.Images.asset.url;
     offerImg.classList.add("offer-img", "sc-1120");
     col1.appendChild(offerImg);
 
@@ -48,18 +44,18 @@ function montarOferta(oferta) {
 
     const offerTitle = document.createElement("h1");
     offerTitle.classList.add("sc-420:!text-[27px]");
-    offerTitle.innerText = oferta.ProdutoNova.Linha[0].Nome;
+    offerTitle.innerText = data.ProdutoNovaLinha.Nome;
     col2.appendChild(offerTitle);
 
     const offerDescription = document.createElement("small");
-    offerDescription.innerText = oferta.Mensagem;
+    offerDescription.innerText = data.Mensagem;
     col2.appendChild(offerDescription);
 
     const buyButton = document.createElement("a");
     buyButton.classList.add("btn", "cursor-pointer", "hover:!text-[white]");
     buyButton.innerHTML = "Compre Agora! &#8594;";
     buyButton.setAttribute("href", "https://api.whatsapp.com/send/?phone=5545920013524&text=Olá, gostaria de saber mais sobre as velas da linha Oh, Happy Day.&type=phone_number&app_absent=0");
-    col2.appendChild(buyButton);
+    col2.append(document.createElement("br"), buyButton);
 
     rowDiv.appendChild(col1);
     rowDiv.appendChild(col2);
